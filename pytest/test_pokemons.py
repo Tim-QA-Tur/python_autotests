ed9d0c481977b2693adb05287029e775059e320b
Timur <5709037@mail.ru>
2025-08-19 10:21:09 +0700

    Update test_pokemons.py

--- pytest/test_pokemons.py
+++ pytest/test_pokemons.py
@@ -4,7 +4,7 @@ import pytest
 URL = 'https://api.pokemonbattle.ru/v2'
 TOKEN = 'введите свой токен'
 HEADER = {'Content-Type':'application/json', 'trainer_token' :TOKEN}
-TRAINER_ID = '28363'
+TRAINER_ID = 'введите свой ID тренера'
 
 def test_status_code():
     response = requests.get(url = f'{URL}/pokemons', params = {'trainer_id' :TRAINER_ID})
@@ -13,8 +13,9 @@ def test_status_code():
 
 def test_pokemon_name():
     response_get_pok_name = requests.get(url = f'{URL}/pokemons', params = {'trainer_id' :TRAINER_ID})
-    assert response_get_pok_name.json()["data"][0]["name"] == 'Chuichui'
+    assert response_get_pok_name.json()["data"][0]["name"] == 'введите имя покемона'
 
 def test_trainer_name():
     response_get_name = requests.get(url = f'{URL}/me', headers=HEADER)
-    assert response_get_name.json()["data"][0]["trainer_name"] == "Барбос"
+    assert response_get_name.json()["data"][0]["trainer_name"] == "введите имя тренера"
+
